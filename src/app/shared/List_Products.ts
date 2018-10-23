e97d3c700b1b8ad3d5e6806ccbe8821a7a6ecc1f
elisoft2018 <johneroblesa@gmail.com>
2018-10-23 18:32:00 -0500

    clase angular

--- src/app/shared/List_Products.ts
+++ src/app/shared/List_Products.ts
@@ -6,7 +6,7 @@ export const PRODUCTS:products[]=[
     description:" de baño",
     price:2,
     expDate: new Date(),
-    image:"https://www.google.com.co/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj6zd7UuJHeAhVprlkKHW_3CaoQjRx6BAgBEAU&url=http%3A%2F%2Fwww.boticadejabon.com%2Fproducto%2Fjabon-artesanal-de-eucalipto-anti-viral-y-desinfectante%2F&psig=AOvVaw1Am70z2Z4WBajD7EZcqb-B&ust=1540001991259075",
+    image:"https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2018/01/05/15151593067050.jpg",
     score:12
     },
     {
@@ -15,7 +15,7 @@ export const PRODUCTS:products[]=[
         description:" desinfectante",
         price:4,
         expDate: new Date(),
-        image:"se perdio",
+        image:"https://jumbocolombiafood.vteximg.com.br/arquivos/ids/785515-1000-1000/7703812002059-1.jpg?v=636489425613530000",
         score:7
     },
     {
@@ -24,7 +24,7 @@ export const PRODUCTS:products[]=[
         description:"aromatizante",
         price:8,
         expDate: new Date(),
-        image:"ahi estaba ayer",
+        image:"https://images.homedepot-static.com/productImages/9cbdde9e-4790-4bdf-a042-e338381320c4/svn/glade-spray-air-fresheners-632769-64_1000.jpg",
         score:2
     }
 ]
